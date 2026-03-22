2a4e5d327f2a9b4d31f86444541a658512c43579
arnauruestesar-hue <arnau.ruestes.ar@gmail.com>
2026-03-22 18:26:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 
 # --- CONFIGURACIÓ DE L'API ---
 # Introdueix la teva API Key de Gemini aquí o configura-la com a variable d'entorn
-GEMINI_API_KEY =st.secrets"AIzaSyCg0vWXdDs1Em8lh4YfeVQ18TalsbUduvI"
+GEMINI_API_KEY ="AIzaSyCg0vWXdDs1Em8lh4YfeVQ18TalsbUduvI"
 client = genai.Client(api_key=GEMINI_API_KEY)
 
 # --- PAS 5: LECTURA DEL CSV DE PREUS ---
